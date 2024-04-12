7ea550c9890ed1d002076a4f980e6111fdee6999
Afoma-tech <156825770+Afoma-tech@users.noreply.github.com>
2024-04-12 18:22:01 -0400

    Defined the EvilCircle class

--- assignment3/part4/main-finished.js
+++ assignment3/part4/main-finished.js
@@ -18,47 +18,51 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+// Create a Shape class
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
-    this.color = color;
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY, size, color);
     this.size = size;
+    this.color = color;
+    this.exists = true;
   }
 
   draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+    if (this.exists) {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+    }
   }
 
   update() {
-    if (this.x + this.size >= width) {
-      this.velX = -Math.abs(this.velX);
-    }
+    if (this.exists) {
+      if (this.x + this.size >= width || this.x - this.size <= 0) {
+        this.velX = -this.velX;
+      }
 
-    if (this.x - this.size <= 0) {
-      this.velX = Math.abs(this.velX);
-    }
+      if (this.y + this.size >= height || this.y - this.size <= 0) {
+        this.velY = -this.velY;
+      }
 
-    if (this.y + this.size >= height) {
-      this.velY = -Math.abs(this.velY);
+      this.x += this.velX;
+      this.y += this.velY;
     }
-
-    if (this.y - this.size <= 0) {
-      this.velY = Math.abs(this.velY);
-    }
-
-    this.x += this.velX;
-    this.y += this.velY;
   }
 
   collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
+    for (const ball of ball) {
+      if (ball.exists && !(this == ball)) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -67,6 +71,33 @@ class Ball {
           ball.color = this.color = randomRGB();
         }
       }
+    }
+  }
+}
+
+// Defining the EvilCircle class
+class EvilCircle extends Shape {
+  constructor(x,y) {
+    super(x,y, 20, 20);
+    this.color = "white";
+    this.size = 10
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width || this.x - this.size <= 0) {
+      this.x -= this.velX
+    }
+
+    if (this.y + this.size >= height || this.y - this.size <= 0) {
+      this.y -= this.velY;
     }
   }
 }
