fdbf7d71555596359936b91e38a85e44a73143ef
Afoma-tech <156825770+Afoma-tech@users.noreply.github.com>
2024-04-12 20:30:43 -0400

    Made it generate random stories

--- assignment3/part1/sillstorygenerator/main.js
+++ assignment3/part1/sillstorygenerator/main.js
@@ -15,16 +15,18 @@ function randomValueFromArray(array){
 
 // Assigned variables 
 
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
-
-const storyText = `It was 94 fahrenheit outside, so ${randomValueFromArray(insertX)} went for a walk. When they got to ${randomValueFromArray(insertY)}, they stared in horror for a few moments, then ${randomValueFromArray(insertZ)}. Bob saw the whole thing, but was not surprised — ${randomValueFromArray(insertX)} weighs 300 pounds, and it was a hot day.`;
 
 // Event listener and partial function definition
 randomize.addEventListener('click', result);
 
 function result() {
+
+  const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+  const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+  const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+  const storyText = `It was 94 fahrenheit outside, so ${randomValueFromArray(insertX)} went for a walk. When they got to ${randomValueFromArray(insertY)}, they stared in horror for a few moments, then ${randomValueFromArray(insertZ)}. Bob saw the whole thing, but was not surprised — ${randomValueFromArray(insertX)} weighs 300 pounds, and it was a hot day.`;
+
   let newStory = storyText;
 
   if (customName.value !== '') {
